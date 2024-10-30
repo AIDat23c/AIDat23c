da1b8c5e9f01e42e2b4e48a44440b12631e69975
Aleksandergreg <alek3691@stud.kea.dk>
2024-10-30 10:47:37 +0100

    Initial commit

---
+++ src/main/java/com/example/aidat23c/AiDat23cApplication.java
@@ -0,0 +1,13 @@
+package com.example.aidat23c;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiDat23cApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AiDat23cApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/aidat23c/AiDat23cApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.aidat23c;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiDat23cApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
